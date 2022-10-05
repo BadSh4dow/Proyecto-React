1aa1efd5c2d92a28b6e1b8abf79e2a808f76bfe2
BadSh4dow <guidocabrera711@gmail.com>
2022-10-04 22:07:09 -0300

    dale que va a salir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
- 
-    </div>
+    <main>
+      <nav className="navBar">
+        <div className="pokeball">
+          <img src ="/Iconos-imagenes/Pokeball.png"/>
+        </div>
+
+        <h1>Pokedex</h1>
+
+        <div className="searchBar">
+          Buscador
+        </div>
+
+        <div className= "arrowFilter">
+          <span>#</span> 
+          <img src="/Iconos-imagenes/Arrow.svg"/>
+        </div>
+
+      </nav>
+    
+      <div className="pokemonView">
+        grilla
+      </div>
+
+    </main>
+  
   );
 }
 
