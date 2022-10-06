4039564614c7aa2f6268123557fe17b48c3050c5
Yayi728 <yaelcalcagno@gmail.com>
2022-10-05 21:54:01 -0300

    dale que so bo

--- src/COMPONENTS/RenderCartas/RenderCartas.jsx
+++ src/COMPONENTS/RenderCartas/RenderCartas.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const RenderCartas= () =>{
     return(
-        <div className="containerBulbasaur">
-            <img className="bulbasaurGrid" src="/Iconos-imagenes/bulbasaur.png"/>
-            <span className="bulbasaurTxt">Bulbasaur</span>
+        <div className="containerGrass">
+            <div className="numberOfPokemon">
+                <span>#001</span>
+            </div>
+            <div className="pokemonImgOne">
+                <img className="grassImg" src="/Iconos-imagenes/bulbasaur.png"/>
+            </div>
+            <div className="pokemonTxtOne">
+                <span className="grassTxt">Bulbasaur</span>
+            </div>    
         </div>
     )
 }
