4835fc25a87e1758748b261aa43b0e72e0ee1611
Yayi728 <yaelcalcagno@gmail.com>
2022-10-11 21:21:14 -0300

    dale que saleeee

--- src/COMPONENTS/Detail/Detail.jsx
+++ src/COMPONENTS/Detail/Detail.jsx
@@ -2,38 +2,44 @@ import React from "react";
 
 const PokemonDetail = () =>{
     return(
-        <div className="BackgroundcolorGreen">
-            <span>Bulbasaur</span>
-            <span>#001</span>
-            <img src="/Iconos-imagenes/Arrow.svg"/>
-            <div className="pokemonDetails">
-                <div className="pokemonType">
-                    <span>Grass</span>
-                    <span>Poison</span>
-                </div>  
-                <div className="pokemonWH">
-                    <span>Wheight</span>
-                    <img src={`/Iconos-imagenes/Weight.svg`} alt="pokemons"/>
-                    <span>Height</span>
-                    <img src={`/Iconos-imagenes/Height.svg`} alt="pokemons"/>
-                    <span>Moves</span>
-                </div>    
-                <p>There is a plant seed on its back right from the days this Pokémon is a bom. The seed slowly grows larger</p>
-                <div className="pokemonStats">
-                    <div>
-                        <ul>HP...</ul>
-                    </div>
-                    <div>
-                        <ul>60...</ul>
-                    </div>
-                    <div>
-                        Barra Verde
-                    </div>
+        <div className="pokeDetalle">
+            <div className="BackgroundcolorGreen">
+                <div className="pokemonVista">
+                    <span>Bulbasaur</span>
+                    <span>#001</span>
+                    <img src="/Iconos-imagenes/Arrow.svg"/>
+                    <img src={`/Iconos-imagenes/aron.png`} alt="pokemons"/>
                 </div>
-            
-            </div>
+                <div className="pokemonDetails">
+                    <div className="pokemonType">
+                        <span>Grass</span>
+                        <span>Poison</span>
+                    </div>  
+                    <div className="pokemonWH">
+                        <span>Wheight</span>
+                        <img src={`/Iconos-imagenes/Weight.svg`} alt="pokemons"/>
+                        <span>Height</span>
+                        <img src={`/Iconos-imagenes/Height.svg`} alt="pokemons"/>
+                        <span>Moves</span>
+                    </div>    
+                    <p>There is a plant seed on its back right from the days this Pokémon is a bom. The seed slowly grows larger</p>
+                    <div className="pokemonStats">
+                        <div>
+                            <ul>HP...</ul>
+                        </div>
+                        <div>
+                            <ul>60...</ul>
+                        </div>
+                        <div>
+                            Barra Verde
+                        </div>
+                    </div>
+                
+                </div>
 
+            </div>
         </div>
+
 
     )
 }
